2e99d9c0e3c30e38bf9b81e20839f874440d1c58
ISMAELGOMEZ80 <jose.gomezg@pca.edu.co>
2021-05-14 15:31:04 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -41,14 +41,14 @@ $app->get('/consulta', function() use($app) {
 
 });
 
-$app->get('/guardarDato/{potenciometro}', function($potenciometro) use($app) {
+$app->get('/guardarDato/{estado}', function($estado) use($app) {
  	$conexion = pg_connect("host=ec2-107-20-153-39.compute-1.amazonaws.com port=5432 dbname=d8r3vjhhkehuv4 user=ybklwjsgmubonm password=9fd44fba109201c501e9ee0bac95f99c73b66dca9f13f0a45c0949f5b0ed9b8a");
 
  	$datetime = date("Y-m-d H:i:s");
 
- 	$datos = array("fecha" => $datetime, "potenciometro" => $potenciometro);
+ 	$datos = array("fecha" => $datetime, "estado" => $estado);
 
- 	$insertar = pg_insert($conexion,"datos",$datos);
+ 	$insertar = pg_insert($conexion,"plazas",$datos);
 
  	return $insertar;
 
