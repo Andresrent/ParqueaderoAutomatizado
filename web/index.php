80234c67b03f81a5ecb8b583ae2408538c0d56dd
heyderpaez <heyderpaez@gmail.com>
2021-05-18 18:28:21 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -41,14 +41,17 @@ $app->get('/consulta', function() use($app) {
 
 });
 
-$app->get('/guardarDato/{node}/{estado}', function($node, $estado) use($app) {
+$app->get('/guardarDato/{node}/{estado}/{senal}', function($node, $estado,$senal) use($app) {
  	$conexion = pg_connect("host=ec2-107-20-153-39.compute-1.amazonaws.com port=5432 dbname=d8r3vjhhkehuv4 user=ybklwjsgmubonm password=9fd44fba109201c501e9ee0bac95f99c73b66dca9f13f0a45c0949f5b0ed9b8a");
 
  	$datetime = date("Y-m-d H:i:s");
 
- 	$datos = array("fecha" => $datetime, "estado" => $estado, "node" => $node);
+ 	$datos = array(	"fecha" => $datetime, 
+ 					"estado" => $estado, 
+ 					"node" => $node,
+ 					"senal" => $senal);
 
- 	$insertar = pg_insert($conexion,"plazass",$datos);
+ 	$insertar = pg_insert($conexion,"plazas",$datos);
 
 
  	return $insertar;
