8db06f72099f611540dfe65c7025724101db9669
heyderpaez <heyderpaez@gmail.com>
2021-05-21 20:37:36 -0500

    test POST con Base de Datos

--- web/index.php
+++ web/index.php
@@ -56,6 +56,24 @@ $app->get('/guardarDato/{node}/{estado}', function($node, $estado) use($app) {
  	return $insertar;
 });
 
+$app->post('/ordenLed', function (Request $request) use ($app) {
+  $estado = $request->get('estado');
+  $node = $request->get('node');
+
+  $conexion = pg_connect("host=ec2-107-20-153-39.compute-1.amazonaws.com port=5432 dbname=d8r3vjhhkehuv4 user=ybklwjsgmubonm password=9fd44fba109201c501e9ee0bac95f99c73b66dca9f13f0a45c0949f5b0ed9b8a");
+
+ 	$datetime = date("Y-m-d H:i:s");
+
+ 	$datos = array(	"fecha" => $datetime, 
+ 					"estado" => $estado, 
+ 					"node" => $node);
+
+ 	$insertar = pg_insert($conexion,"plazas",$datos);
+
+
+ 	return $insertar;
+});
+
 $app->get('/consultarPlaza/{plaza}', function($plaza) use($app) {
  	$conexion = pg_connect("host=ec2-107-20-153-39.compute-1.amazonaws.com port=5432 dbname=d8r3vjhhkehuv4 user=ybklwjsgmubonm password=9fd44fba109201c501e9ee0bac95f99c73b66dca9f13f0a45c0949f5b0ed9b8a");
 
@@ -65,28 +83,6 @@ $app->get('/consultarPlaza/{plaza}', function($plaza) use($app) {
  	return $datos[2];
 });
 
-$app->get('/liberarEspacio/{tabla}', function($tabla) use($app) {
- 	$conexion = pg_connect("host=ec2-107-20-153-39.compute-1.amazonaws.com port=5432 dbname=d8r3vjhhkehuv4 user=ybklwjsgmubonm password=9fd44fba109201c501e9ee0bac95f99c73b66dca9f13f0a45c0949f5b0ed9b8a");
 
- 	$limiteDB = 60;
- 	$datosSalvados = 20;
-
- 	$query = "SELECT * FROM ". $tabla ." ORDER BY id DESC";
- 	$consulta = pg_query($conexion, $query);
- 	$filas = pg_num_rows($consulta);
- 	$respuesta=pg_fetch_all($consulta);
- 	$id=$respuesta[0]["id"];
-
- 	$index = $id - datosSalvados;
-
- 	if($filas>limiteDB){
- 		$query2 = 'DELETE FROM '. $tabla .' WHERE id <='.$index;
- 		$consulta2 = pg_query($conexion, $query2);
- 		return $consulta2;
- 	}
- 	else{
- 		return "Aún no es necesario borrar datos";
- 	}
-});
 
 $app->run();
