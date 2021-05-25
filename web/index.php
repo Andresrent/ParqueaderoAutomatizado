f8ea849ac17f5dfbeab7b5d3a9a44dd35703404b
heyderpaez <heyderpaez@gmail.com>
2021-05-24 19:39:29 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -30,16 +30,6 @@ $app->get('/login', function() use($app) {
   return ('Estamos construyendo la página de login');
 });
 
-$app->get('/consulta', function() use($app) {
- 	$conexion = pg_connect("host=ec2-107-20-153-39.compute-1.amazonaws.com port=5432 dbname=d8r3vjhhkehuv4 user=ybklwjsgmubonm password=9fd44fba109201c501e9ee0bac95f99c73b66dca9f13f0a45c0949f5b0ed9b8a");
-
- 	$consulta = pg_query($conexion, "SELECT * FROM menu");
-
- 	$detalle = pg_fetch_result($consulta,2,2);
-
- 	return $detalle;
-
-});
 
 $app->get('/guardarDato/{node}/{estado}', function($node, $estado) use($app) {
  	$conexion = pg_connect("host=ec2-107-20-153-39.compute-1.amazonaws.com port=5432 dbname=d8r3vjhhkehuv4 user=ybklwjsgmubonm password=9fd44fba109201c501e9ee0bac95f99c73b66dca9f13f0a45c0949f5b0ed9b8a");
@@ -90,13 +80,37 @@ $app->post('/guardarTemp', function (Request $request) use ($app) {
  	return $insertar;
 });
 
+
 $app->get('/consultarPlaza/{plaza}', function($plaza) use($app) {
  	$conexion = pg_connect("host=ec2-107-20-153-39.compute-1.amazonaws.com port=5432 dbname=d8r3vjhhkehuv4 user=ybklwjsgmubonm password=9fd44fba109201c501e9ee0bac95f99c73b66dca9f13f0a45c0949f5b0ed9b8a");
 
- 	$query = "SELECT * FROM plazas WHERE node=".$plaza."ORDER BY fecha DESC LIMIT 1";
- 	$consulta = pg_query($query);
- 	$datos = pg_fetch_row($consulta);
- 	return $datos[2];
+ 	if(is_int($plaza)){
+      $query = "SELECT * FROM plazas WHERE node=".$plaza."ORDER BY fecha DESC LIMIT 1";
+    $consulta = pg_query($query);
+    $datos = pg_fetch_row($consulta);
+    return $datos[2];
+   }
+   if($plaza=="todas"){
+      for($=1;$i<=2;i++){
+          $query = "SELECT * FROM plazas WHERE node=".$i."ORDER BY fecha DESC LIMIT 1";
+          $consulta = pg_query($query);
+          $datos = pg_fetch_row($consulta);
+          $estados[$i]=$datos[2];
+      }
+      return $estados;
+   }
+   if($plaza=="disponibles"){
+      $totalDisponible=0;
+      for($=1;$i<=2;i++){
+          $query = "SELECT * FROM plazas WHERE node=".$i."ORDER BY fecha DESC LIMIT 1";
+          $consulta = pg_query($query);
+          $datos = pg_fetch_row($consulta);
+          if($datos[2]==0){
+            $totalDisponible=$totalDisponible+1;
+          }
+      }
+      return $totalDisponible;
+   }
 });
 
 
