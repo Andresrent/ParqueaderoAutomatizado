70788a6d0ac3b633c2f4b72a3074a0c652f5f8ae
heyderpaez <heyderpaez@gmail.com>
2021-05-25 20:07:05 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -87,11 +87,17 @@ $app->get('/consultarPlaza/{plaza}', function($plaza) use($app) {
  	$conexion = pg_connect("host=ec2-107-20-153-39.compute-1.amazonaws.com port=5432 dbname=d8r3vjhhkehuv4 user=ybklwjsgmubonm password=9fd44fba109201c501e9ee0bac95f99c73b66dca9f13f0a45c0949f5b0ed9b8a");
 
  	if(is_int($plaza)){
+    try{
       $query = "SELECT * FROM plazas WHERE node=".$plaza."ORDER BY fecha DESC LIMIT 1";
-    $consulta = pg_query($conexion,$query);
-    $datos = pg_fetch_row($consulta);
-    return $datos[2];
-   }
+      $consulta = pg_query($conexion,$query);
+      $datos = pg_fetch_row($consulta);
+      return $datos[2];
+    }
+    catch (Exception $e) {
+          return $e->getMessage();
+    }
+  }
+
    if($plaza=="todas"){
       $estados = array();
 
@@ -132,6 +138,8 @@ $app->get('/consultarPlaza/{plaza}', function($plaza) use($app) {
       }
       return $totalDisponible;
    }
+
+
 });
 
 
