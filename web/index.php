98c232ed441dab122edf15f89e68bef64846354c
heyderpaez <heyderpaez@gmail.com>
2021-05-25 19:48:22 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -101,7 +101,8 @@ $app->get('/consultarPlaza/{plaza}', function($plaza) use($app) {
           $consulta = pg_query($conexion,$query);
           $datos = pg_fetch_row($consulta);
           $index=$i+1;
-          $estados = array_merge($estados, array(strval($index)=>$datos[2]));
+          $estados = array_merge($estados, array("plaza"=>$i,
+            "estado"=>$datos[2]));
           }
 
           $jsonResult = json_encode($estados, JSON_PRETTY_PRINT | JSON_FORCE_OBJECT);
